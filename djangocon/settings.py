3228605c9083b030ef7ef6c9322238369f444994
Baptiste Mispelon <bmispelon@gmail.com>
2015-10-16 01:16:47 +0200

    Fixed TEMPLATES setting when DEBUG=False

--- djangocon/settings.py
+++ djangocon/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
 ]
 
 if not DEBUG:
+    del TEMPLATES[0]['APP_DIRS']
     TEMPLATES[0]['OPTIONS']['loaders'] = [
         ('django.template.loaders.cached.Loader', [
             'django.template.loaders.filesystem.Loader',
