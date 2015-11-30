f8eb3cc5613a67d499a994a5b22ba2e76e17191a
Baptiste Mispelon <bmispelon@gmail.com>
2015-11-30 11:09:15 +0100

    Fixed missing trailing slash in urls

--- cfp/urls.py
+++ cfp/urls.py
@@ -5,6 +5,6 @@ from cfp.views import LandingView, CreateView, ThankYouView
 
 urlpatterns = [
     url(r'^$', LandingView.as_view(), name='landing'),
-    url(r'^propose$', CreateView.as_view(), name='propose'),
+    url(r'^propose/$', CreateView.as_view(), name='propose'),
     url(r'^thanks/$', ThankYouView.as_view(), name='thanks'),
 ]
