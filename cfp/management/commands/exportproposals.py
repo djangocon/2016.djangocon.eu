845b8ba2f316e056477f3e136f535d2d88d5ef2c
Baptiste Mispelon <bmispelon@gmail.com>
2016-01-07 19:26:00 +0100

    Added command to export proposals to CSV

---
+++ cfp/management/commands/exportproposals.py
@@ -0,0 +1,49 @@
+import argparse
+import csv
+
+from django.core.exceptions import ValidationError
+from django.core.management.base import BaseCommand
+from django.forms.fields import DateTimeField
+
+from cfp.models import Proposal
+
+
+def datetimetype(s):
+    f = DateTimeField()
+    try:
+        return f.to_python(s)
+    except ValidationError as e:
+        raise argparse.ArgumentTypeError("Unrecognized datetime format") from e
+
+
+class Command(BaseCommand):
+    help = 'Dump the proposals as CSV to stdout'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--since', '-s', type=datetimetype, metavar='SINCE', help="Limit to proposals submitted after SINCE.")
+
+    def handle(self, *args, **options):
+        queryset = Proposal.objects.all()
+        if options['since']:
+            queryset = queryset.filter(submitted_on__gte=options['since'])
+
+        writer = csv.writer(self.stdout)
+
+        writer.writerow((
+            'ID',
+            'Title',
+            'Description',
+            'Audience',
+            'Skill level',
+            'Notes',
+        ))
+
+        for proposal in queryset:
+            writer.writerow((
+                proposal.pk,
+                proposal.title,
+                proposal.description,
+                proposal.audience,
+                proposal.get_skill_level_display(),
+                proposal.notes,
+            ))
