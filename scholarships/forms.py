904b848e5f4a9c8892cdaa77d250e3b94ee025c7
Baptiste Mispelon <bmispelon@gmail.com>
2016-01-12 11:25:28 +0100

    Added email confirmation sending after scholarship application

--- scholarships/forms.py
+++ scholarships/forms.py
@@ -1,6 +1,29 @@
+from django.core.mail import send_mail
 from django import forms
+from django.template import Context, Template
 
 from .models import Application
+
+CONFIRMATION_EMAIL_TEMPLATE = """Hi {{ form.name.value }},
+
+
+This is an automated email to confirm that we have received your application
+for the DjangoCon Europe 2016 scholarship program.
+
+
+Here is the data you submitted to us:
+{% for field in form %}{{ field.label }}:
+{{ field.value }}
+
+
+{% endfor %}
+We will contact you with the result of your application on February 1st.
+In the meantime, feel free to contact us if you have any questions: 2016@djangocon.eu
+
+
+All the best,
+The DjangoCon Europe 2016 team.
+"""
 
 
 class ApplicationForm(forms.ModelForm):
@@ -37,3 +60,15 @@ class ApplicationForm(forms.ModelForm):
                 continue
             f.widget.attrs.setdefault('required', True)
             f.label = '{} *'.format(f.label)
+
+    def save(self, commit=True):
+        application = super(ApplicationForm, self).save(commit)
+        self.send_confirmation_email()
+        return application
+
+    def send_confirmation_email(self):
+        subject = '[DjangoCon Europe 2016] Confirmation of your scholarship application'
+        body = Template(CONFIRMATION_EMAIL_TEMPLATE).render(Context({'form': self}))
+        to = self.cleaned_data['email']
+        from_ = 'DjangoCon Europe 2016 <2016@djangocon.eu>'
+        return send_mail(subject, body, from_, [to])
