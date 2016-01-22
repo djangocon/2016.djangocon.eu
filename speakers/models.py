c7fd082dd360248e727588c3c5ea46ded4140bb4
Baptiste Mispelon <bmispelon@gmail.com>
2016-01-22 18:04:55 +0100

    Improved display of speakers

--- speakers/models.py
+++ speakers/models.py
@@ -1,7 +1,10 @@
 import requests
 
 from django.core.files import File
+from django.core.urlresolvers import reverse
 from django.db import models
+from django.template.defaultfilters import urlencode
+from django.utils.html import format_html
 from django.utils import timezone
 
 
@@ -19,3 +22,22 @@ class Speaker(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_picture_html(self, size=285):
+        if self.picture:
+            src = self.picture.url
+        else:
+            src = 'http://api.adorable.io/avatars/%s/%s' % (size, urlencode(self.name))
+
+        return format_html('<img alt="{name}" src="{src}">', name=self.name, src=src)
+
+    def get_absolute_url(self):
+        return reverse('speakers:list') + '#speaker-%s' % self.pk
+
+    @property
+    def twitter_url(self):
+        return 'https://twitter.com/%s' % self.twitter
+
+    @property
+    def github_url(self):
+        return 'https://github.com/%s' % self.github
