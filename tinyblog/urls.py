d0ab115e089aed0b2f2d8362725d6db72f4fcae0
Ola Sitarska <ola@sitarska.com>
2016-01-23 14:20:00 +0000

    Fix slug pattern in urls for tinyblog

--- tinyblog/urls.py
+++ tinyblog/urls.py
@@ -5,5 +5,5 @@ from .views import IndexView, ArticleView
 
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^article/(?P<slug>\w+)/$', ArticleView.as_view(), name='article'),
+    url(r'^article/(?P<slug>[-\w]+)/$', ArticleView.as_view(), name='article'),
 ]
