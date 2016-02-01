17c5df2c6cb224ec1f5d5f88199aa9506f3e6c90
Baptiste Mispelon <bmispelon@gmail.com>
2016-02-01 17:32:13 +0100

    Changed name of FAQ list page

--- faq/urls.py
+++ faq/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from faq.views import FAQListView
 
 urlpatterns = [
-    url(r'^$', FAQListView.as_view(), name='faq_list'),
+    url(r'^$', FAQListView.as_view(), name='list'),
 ]
