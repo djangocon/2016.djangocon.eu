cc8ad26a900f498969b5b017b5bb2ce8f7f26259
ana-balica <ana.balica@gmail.com>
2016-02-04 17:41:25 +0000

    Adjust style of Gold sponsor blocks

--- sponsors/models.py
+++ sponsors/models.py
@@ -82,7 +82,7 @@ class Sponsor(models.Model):
     @property
     def logo_html(self):
         if self.logo:
-            return format_html('<img class="logo" src="{0}" alt="{1}" title="{1}"/>', self.logo.url, self.name)
+            return format_html('<img src="{0}" alt="{1}" title="{1}"/>', self.logo.url, self.name)
         return format_html('<span class="nologo">{0}</span>', self.name)
 
     def get_toolbox(self, user):
